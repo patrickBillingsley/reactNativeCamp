d31c7fd73643e99a50f8252d20197ebb8ac47d29
Patrick Billingsley <prbillingsley89@gmail.com>
2021-07-03 11:29:37 -0500

    Week 3 Workshop

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -25,6 +25,7 @@ function RenderCampsite(props) {
     const view = React.createRef();
 
     const recognizeDrag = ({dx}) => (dx < -200) ? true : false;
+    const recognizeComment = ({dx}) => (dx > 200) ? true : false;
 
     const panResponder = PanResponder.create({
         onStartShouldSetPanResponder: () => true,
@@ -52,6 +53,8 @@ function RenderCampsite(props) {
                     ],
                     { cancelable: false }
                 )
+            } else if(recognizeComment(gestureState)) {
+                props.onShowModal();
             }
             return true;
         }
